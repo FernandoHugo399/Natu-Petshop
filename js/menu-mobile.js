7d2f5ef390e520662d428196e024f5cba3fcaf94
Hugo Fernando <huginho888@outlook.com>
2021-09-24 18:02:15 -0300

    otimizando codigo

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,13 +1,13 @@
 var menu = document.querySelectorAll('.icon-menu')[0];
 var moba = document.querySelectorAll('.menu-mobile')[0];
 
-document.body.onresize = function() {
+addEventListener('resize', () =>{
     if (document.body.clientWidth >= 1205){
         if(moba.classList.contains('mobah')){
             moba.classList.remove('mobah')
         }
     }
-};
+})
 
 menu.addEventListener('click', ()=>{
     if(moba.classList.contains('mobah')){
