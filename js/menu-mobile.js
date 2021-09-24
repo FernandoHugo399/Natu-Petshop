2c88e56bec03fec11a97ee615f0e4b5c5189acd5
Hugo Fernando <huginho888@outlook.com>
2021-09-24 14:21:33 -0300

    menu v3

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,10 +1,9 @@
-var menu = document.querySelectorAll('.menu-mobile')[0];
-var moba = document.querySelectorAll('.menu-mobile ul')[0];
+var menu = document.querySelectorAll('.icon-menu')[0];
+var moba = document.querySelectorAll('.menu-mobile')[0];
 menu.addEventListener('click', ()=>{
     if(moba.classList.contains('mobah')){
-        moba.classList.remove('mobah');
+        moba.classList.remove('mobah')
     } else {
         moba.classList.add('mobah');
     }
-    
 })
