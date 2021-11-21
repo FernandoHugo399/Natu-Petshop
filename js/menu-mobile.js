c685891cd3993cfe5e984d43f816d92a65315ac7
Hugo Fernando <huginho888@outlook.com>
2021-11-21 20:26:24 -0300

    Adicionando links

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -2,7 +2,7 @@ var menu = document.querySelectorAll('.icon-menu')[0];
 var moba = document.querySelectorAll('.menu-mobile')[0];
 
 addEventListener('resize', () =>{
-    if (document.body.clientWidth >= 1205){
+    if (document.body.clientWidth >= 1295){
         if(moba.classList.contains('mobah')){
             moba.classList.remove('mobah')
         }
